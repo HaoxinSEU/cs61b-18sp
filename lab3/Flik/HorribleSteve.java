3025a247ee30fe3a46825ed43f4287ae5a8f3097
Haoxin Sun <sun.haoxin@foxmail.com>
2021-03-15 20:32:19 +0800

    finish lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -3,6 +3,7 @@ public class HorribleSteve {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
+                System.out.println("Now i is " + i + " and j is " + j + " isSameNumber() gives: " + Flik.isSameNumber(i, j));
                 break; // break exits the for loop!
             }
         }
